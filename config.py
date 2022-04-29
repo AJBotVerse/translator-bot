d11e65d855a7595180ece1405844736f356c77bc
Abhijeet <ajtimepyro@gmail.com>
2022-04-29 13:46:24 +0530

    Group and user data added in config file

--- config.py
+++ config.py
@@ -18,3 +18,45 @@ class Config(object):
     # Your(owner's) telegram id
     OWNER_ID = int(environ.get("OWNER_ID", None))
 
+    # Group and User Data
+    GROUP_USER_DATA = dict(environ.get("GROUP_USER_DATA", ""))
+    """
+    For 'GROUP_USER_DATA', read it carefully.
+    create a dictionary or json data, and add data like this
+
+    Syntax Below:-
+    {
+        GroupId1 : {
+            userid1 : lang1,
+            userid2 : lang2,
+            userid3 : lang3,
+            ....
+        },
+        GroupId2 : {
+            userid1 : lang1,
+            userid2 : lang2,
+            userid3 : lang3,
+            ....
+        },
+        ....
+    }
+
+    eg.,
+    {
+        -610274425 : {
+            5105154963 : 'hi',
+            4175164933 : 'eng'
+        },
+        -100610274425: {
+            2125464768 : 'ru',
+            3135566963 : 'eng'
+        }
+    }
+
+    Now in this example,
+    In group -610274425, user with userid 5105154963, his message will translate to hindi,
+    And user with userid 4175164933, his message will translate to english.
+
+    Hope you understand it.
+    """
+
